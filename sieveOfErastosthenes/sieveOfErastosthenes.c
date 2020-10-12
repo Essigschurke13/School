4195dd7fd956c093cc650e801e582ca430ef2121
Benedikt Stonig <bstonig@gmail.com>
2020-10-12 19:08:08 +0200

    finished sieve of erastosthenes

--- sieveOfErastosthenes/sieveOfErastosthenes.c
+++ sieveOfErastosthenes/sieveOfErastosthenes.c
@@ -1,14 +1,45 @@
+/**
+ *
+ * Program: Sieve of Erastosthenes
+ * Author: Benedikt Stonig 5AHEL
+ * Date: 11.10.2020
+ * Description: This Program calculates prime numbers using the sieve of erastosthenes.
+ *
+ */
+
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 int main()
 {
-    int numbers[498];
+    //initalize bool array for primes
+    bool primeNumbers[500];
     for (int i = 2; i <= 500; i++)
     {
-        numbers[i]=i;
+        primeNumbers[i]=true;
     }
 
-    
+    //calculate primes with the sieve of erastosthenes
+    for (int i = 2; i <= 23; i++)
+    {
+        if (primeNumbers[i])
+        {
+            for (int j = i*i; j <= 500; j += i)
+            {
+                primeNumbers[j] = false;
+            }
+        }
+    }
+
+    //print the result
+    for (int i = 2; i < 500; i++)
+    {
+        if (primeNumbers[i])
+        {
+            printf("%i ", i);
+        }
+    }
 
     return 0;
 }
